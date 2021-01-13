065ee9bbf61c66781ecdeac405fd44246186b3f1
Juan Collaguazo <juan.collaguazo01@epn.edu.ec>
2021-01-13 00:01:51 -0500

    #23 validacion de datos ComentController validacion de categoria de articulo validacion de titulo unico con exepcion de update

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -10,10 +10,6 @@ use Illuminate\Support\Facades\Validator;
 
 class ArticleController extends Controller
 {
-    private static $rules = [
-        'title' => 'required|string|unique:articles|max:255',
-        'body' => 'required',
-    ];
     private static $messages = [
         'required' => 'El campo :attribute es obligatorio.',
         'body.required' => 'Body no valido'
@@ -28,7 +24,10 @@ class ArticleController extends Controller
     }
     public function store(Request $request)
     {
-        $request->validate(self::$rules,self::$messages);
+        $request->validate([
+        'title' => 'required|string|unique:articles|max:255',
+        'body' => 'required',
+        'category_id' => 'required|exists:categories,id'],self::$messages);
 //        $validator = Validator::make($request->all(), [
 //            'title' => 'required|string|unique:articles|max:255',
 //            'body' => 'required|string'
@@ -39,10 +38,14 @@ class ArticleController extends Controller
 //        }
 
         $article = Article::create($request->all());
-        return response()->json(new ArticleResource($article), 201);
+        return response()->json($article, 201);
     }
     public function update(Request $request,  Article $article)
     {
+        $request->validate([
+            'title' => 'required|string|unique:articles,title,'.$article->id.'|max:255',
+            'body' => 'required',
+            'category_id' => 'required|exists:categories,id'],self::$messages);
         $article->update($request->all());
         return response()->json($article, 200);
     }
